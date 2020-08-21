b3500aaee3474ceac893cf217f23d75e8382fa73
jcbgrub <jacob.hp.grub@gmail.com>
2020-08-21 10:08:23 +0000

    setup  service3

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -6,10 +6,12 @@ import random
 #generates a the name of a random author 
 @app.route('/get_num_one',methods=['GET'])
 def get_name1():
-    name2= name_four[random.randint(1,10)]
+    name1 = ['2','3','4','6','9']
+    name1= name1[random.randrange(6)]
     return Response(author,mimetype='text/plain')
 
 @app.route('/get_num_two',methods=['GET'])
 def get_name2():
-    name2 = name2[random.randint(10,100)]
+    name2 = ['11','22','43','69','99']
+    name2 = name2[random.randrange(6)]
     return Response(author,mimetype='text/plain')
