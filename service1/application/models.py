951ce8d313b65ad53ce8f3f782a706340296eb13
jcbgrub <jacob.hp.grub@gmail.com>
2020-08-22 09:17:04 +0000

    working on docker

--- service1/application/models.py
+++ service1/application/models.py
@@ -1,7 +1,11 @@
 from application import db
 from datetime import datetime
-# export DATABASE_URI="mysql+pymysql://root:root@35.246.10.208:3306/animal_db"
-# export SECRET_KEY=fasdfadsfadsfwaefsadv43tw4
+# export DATABASE_URI="mysql+pymysql://root:root@35.246.94.253:3306/emaildb"
+# export TEST_DATABASE_URI="mysql+pymysql://root:root@35.246.94.253:3306/emaildbtest"
+# SECRET_KEY="fasdfadsfadsfwaefsadv43tw777774"
+# TEST_SECRET_KEY="fasdfadsfadsfwaefsadv43tw4"
+
+
 
 class email(db.Model):
 	id = db.Column(db.Integer, primary_key=True,autoincrement=True)
