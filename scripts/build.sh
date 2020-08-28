68589d6c120a6e24566a5f9a3b93742b0a4b07c0
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 13:19:33 +0000

    changed user inventory

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,8 @@ export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI} 
 sudo docker-compose down --rmi all
 sudo docker-compose build
+sudo docker login
 sudo docker-compose push
-docker login
-docker-compose build
 
 # docker push sch0tterfoinfoin/service1:latest
 # docker push sch0tterfoinfoin/service2:latest
