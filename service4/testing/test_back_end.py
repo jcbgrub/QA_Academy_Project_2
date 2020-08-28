8a1a8397164bc3f2cb83fc66830c604408bbb5a8
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 08:46:26 +0000

    testing

--- service4/testing/test_back_end.py
+++ service4/testing/test_back_end.py
@@ -28,8 +28,8 @@ class TestResponse(TestBase):
 			response = self.client.post(url_for('get_email'),data='test1234567')
 			self.assertIn(b'test1234567@spacerocket.com', response.data)
 
-	def test_else(self):
-		with patch('requests.get') as g:
-			g.return_value.text = "3"
-			# response = self.client.post(url_for('get_email'),data='3')
-			self.assertIn(b'this should not be here', response.data)
+	# def test_else(self):
+	# 	with patch('requests.get') as g:
+	# 		g.return_value.text = "3"
+	# 		# response = self.client.post(url_for('get_email'),data='3')
+	# 		self.assertIn(b'this should not be here', response.data)
