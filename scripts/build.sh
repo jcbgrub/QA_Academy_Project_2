9a39209821e5ea8fee4f563dd6480dbedb6050dc
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 13:49:01 +0000

    changed user inventory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 # docker
 # sudo chmod 666 /var/run/docker.sock
-# export SECRET_KEY=${SECRET_KEY} 
-# export DATABASE_URI=${DATABASE_URI}
+export SECRET_KEY=${SECRET_KEY} 
+export DATABASE_URI=${DATABASE_URI}
+export TEST_SECRET_KEY=${TEST_SECRET_KEY} 
+export TEST_DATABASE_URI=${TEST_DATABASE_URI}
 
 docker-compose down --rmi all
 docker-compose build
