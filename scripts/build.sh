4a6f238fd7976506f0ccbfe4f00ebc1a7a249bcc
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 13:53:14 +0000

    changed user inventory

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,3 @@ docker-compose down --rmi all
 docker-compose build
 docker login 
 docker-compose push
-
-# docker push sch0tterfoinfoin/service1:latest
-# docker push sch0tterfoinfoin/service2:latest
-# docker push sch0tterfoinfoin/service3:latest
-# docker push sch0tterfoinfoin/service4:latest
