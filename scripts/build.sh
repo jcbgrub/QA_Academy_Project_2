4da245fa29a2c262079ed62af5239854b20e9af4
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 10:59:53 +0000

    setting req

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-# make sure ~/.local/bin exists and is on your PATH
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-# install ansible with pip
-pip3 install --user ansible
-ansible-playbook -i inventory ansible/playbook.yaml
 # docker
 sudo docker-compose down --rmi all
 sudo docker-compose build
---
+++ scripts/configure.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# make sure ~/.local/bin exists and is on your PATH
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+# install ansible with pip
+pip3 install --user ansible
+ansible-playbook -i inventory ansible/playbook.yaml
