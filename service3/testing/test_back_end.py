7aadd79d4ff89e3e8054fe1646168266f154622c
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 08:26:56 +0000

    testing

--- service3/testing/test_back_end.py
+++ service3/testing/test_back_end.py
@@ -5,14 +5,12 @@ from flask_testing import TestCase
 from application import app
 
 class TestBase(TestCase):
-
     def create_app(self):
         return app
 
 class TestResponse(TestBase):
-
-    def test_team_name(self):
-        with patch('random.randrange') as r:
+    def test_team_number(self):
+        with patch('random.randint') as r:
             r.return_value = 1
             response = self.client.get(url_for('get_number'))
             self.assertIn(b'1', response.data)
