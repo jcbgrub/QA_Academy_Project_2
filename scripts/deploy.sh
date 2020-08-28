684197c738ae8550fd373c4287524d19559df8c9
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 14:42:30 +0000

    changed user inventory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 ssh sfia-manager << EOF
-ls
-pwd
-whoami
+git clone https://github.com/jcbgrub/SFIA_Project_2.git
 cd SFIA_Project_2/
 export SECRET_KEY=${SECRET_KEY} 
 export DATABASE_URI=${DATABASE_URI} 
 docker stack deploy --compose-file docker-compose.yaml sfia2
+cd .. 
+rm -r SFIA_Project_2/
 EOF
