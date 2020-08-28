934c0a4a06e30dd43a799f3e641fa73cd98b8a80
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-28 08:38:35 +0000

    testing

--- service4/testing/test_back_end.py
+++ service4/testing/test_back_end.py
@@ -14,8 +14,18 @@ class TestResponse(TestBase):
 	# We will mock a response of 1 and test that we get football returned.
 		with patch('requests.get') as g:
 			g.return_value.text = "test12"
-			response = self.client.get(url_for('get_email'))
+			response = self.client.post(url_for('get_email'),data='test12')
 			self.assertIn(b'test12@sputnik.com', response.data)
+
+    # def test_team_slogan(self): 
+    #     with patch('requests.get') as i:
+    #             i.return_value.text = 'London'
+    #             with patch('random.randrange') as r:
+    #                 r.return_value = 1
+    #                 response = self.client.post(
+    #                 url_for('get_slogan'),
+    #                 data = 'London')
+    #                 self.assertIn(b'Win from Within.', response.data)
 
 
 		#         def test_team_slogan(self): 
