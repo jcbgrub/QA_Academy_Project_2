7ffba6faa2c9e24e5b4bc2686a2d0ee59142e645
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-29 21:31:26 +0000

    implementation test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,10 @@ cd SFIA_Project_2/
 export SECRET_KEY=${SECRET_KEY} 
 export DATABASE_URI=${DATABASE_URI} 
 # docker stack rm sfia2
+docker pull sch0tterfoinfoin/service1:latest
+docker pull sch0tterfoinfoin/service2:latest
+docker pull sch0tterfoinfoin/service3:latest
+docker pull sch0tterfoinfoin/service4:latest
 docker stack deploy --compose-file docker-compose.yaml sfia2
 docker service ls
 cd .. 
