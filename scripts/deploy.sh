f1129118948ef605d6b034a9203b987359c2b4ad
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-29 12:50:25 +0000

    implementation test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # SSH to the manager, clone GIT hub, export environment variables, deploy docker stock and remove git repo afterwards
 ssh sfia-manager << EOF
+pwd
 git clone https://github.com/jcbgrub/SFIA_Project_2.git
 cd SFIA_Project_2/
 export SECRET_KEY=${SECRET_KEY} 
