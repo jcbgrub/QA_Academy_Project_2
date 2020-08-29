64a59ea053f250453e546c169cd910f4da4f89f4
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-29 11:33:52 +0000

    implementation test

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -4,19 +4,19 @@ import requests
 
 import random
 
-#generates a the name of a random name  
-# @app.route('/get_name',methods=['GET'])
-# def get_name():
-#     name = ['Keynes','Friedrich','Liebknecht','Vladimir','Rosa']
-#     name = name[random.randint(0,4)]
-#     # return Response(name,mimetype='text/plain')
-#     return name
-
-#generates a the name of a random name in capital
+# generates a the name of a random name  
 @app.route('/get_name',methods=['GET'])
 def get_name():
-    name = ['KEYNES','FRIEDRICH','LIEBKNECHT','VLADIMIR','ROSA']
+    name = ['Keynes','Friedrich','Liebknecht','Vladimir','Rosa']
     name = name[random.randint(0,4)]
-    
     # return Response(name,mimetype='text/plain')
     return name
+
+#generates a the name of a random name in capital
+# @app.route('/get_name',methods=['GET'])
+# def get_name():
+#     name = ['KEYNES','FRIEDRICH','LIEBKNECHT','VLADIMIR','ROSA']
+#     name = name[random.randint(0,4)]
+    
+#     # return Response(name,mimetype='text/plain')
+#     return name
