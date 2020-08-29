047726d2203134ca1c6df793fb6e0f08d7cce17f
jcbgrub <jacob_hp_grub@gmail.com>
2020-08-29 11:27:58 +0000

    implementation test

--- service3/application/routes.py
+++ service3/application/routes.py
@@ -4,15 +4,15 @@ from application import app
 import random
 
 #  generate random number 1-99
-# @app.route('/get_number',methods=['GET'])
-# def get_number():
-#     number = random.randint(0,100)
-#     # return Response(number,mimetype='text/plain')
-#     return str(number)
- 
- #  generate random number 10 and 100 by 5
 @app.route('/get_number',methods=['GET'])
 def get_number():
-    number = random.randint(666,6666)
+    number = random.randint(0,100)
     # return Response(number,mimetype='text/plain')
     return str(number)
+ 
+#  #  generate random number 10 and 100 by 5
+# @app.route('/get_number',methods=['GET'])
+# def get_number():
+#     number = random.randint(666,6666)
+#     # return Response(number,mimetype='text/plain')
+#     return str(number)
