e0f4694a2aa59276c23a6f6c6278d1edc197ecdf
jcbgrub <jacob_hp_grub@gmail.com>
2020-09-01 07:51:34 +0000

    init new vms

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -6,7 +6,7 @@ import random
 #generates a the name of a random name with 4 letters 
 @app.route('/get_name',methods=['GET'])
 def get_name():
-	name = ['Keynes','Friedrich','Liebknecht','Vladimir','Rosa']
+	name = ['KEynes','Friedrich','LIebknecht','Vladimir','Rosa']
 	name = name[random.randint(0,4)]
 	# return Response(name,mimetype='text/plain')
 	return name
